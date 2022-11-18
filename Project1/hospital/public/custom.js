92df9a8743997478b214795faa37f71af620b5d4
kiryu <114374346+AzrielRB@users.noreply.github.com>
2022-11-18 11:22:25 +0700

    PROJECT UTS PEMWEB

---
+++ Project1/hospital/public/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    $('#slideshow .slick').slick({
+        autoplay: true,
+        autoplaySpeed: 3000, // autoplaySpeed: 1000, or             autoplaySpeed: 2000,
+        dots: true,
+        arrows : false,
+    });
+});
+$(document).ready(() => {
+    $('#userReview .slick').slick({
+            autoplay: true,
+            autoplaySpeed: 3000,
+            dots: true,
+            arrows : false,
+    });
+});
