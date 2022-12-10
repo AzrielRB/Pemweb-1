b776dd2ec4e0033128a920116e5f8891b8fcbc76
kiryu <114374346+AzrielRB@users.noreply.github.com>
2022-12-10 19:20:08 +0700

    Add files via upload

---
+++ p10_azrielrb_ti05/form.js
@@ -0,0 +1,9 @@
+function kirim(){
+let nama_pelanggan = document.getElementById("nama_pelanggan").value;
+let email = document.getElementById("email").value;
+if(nama_pelanggan =="ilman"){
+    alert("Login Success");
+} else{
+    alert("login gagal");
+}
+}
